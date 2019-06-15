176fe64da8cd21eed31eeabd80db31791a7eec94
Anna <annavinogradova9901@gmail.com>
2019-06-15 13:31:59 +0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const auth = require('./auth');
+const user = require('./user');
+const geo = require('./geo');
+const site = require('./site');
+
+module.exports = {
+  auth,
+  user,
+  geo,
+  site
+};
